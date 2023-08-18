f49c541244f9c45d3fdea3ae549f368e546821ce
pyosoo <snstkfka02@latestk.com>
2023-08-18 19:10:23 +0900

    security commit1

---
+++ src/Interfaces.ts
@@ -0,0 +1,13 @@
+interface keyInterface {
+    value: string;
+    type: string;
+}
+export interface keypadInterface {
+    id: number;
+    keys: keyInterface[][];
+}
+
+export interface keyItemInterface {
+    value: string;
+    type: string;
+}
---
+++ src/styledItems.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const InputDiv = styled.div`
+    border-radius: 5px;
+    border: 1px solid gray;
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    vertical-align: center;
+`
+
+export const KeyDiv = styled.div`
+    width:50px;
+    height: 50px;
+    line-height: 50px;
+    vertical-align: middle;
+    text-align: center;
+    border-radius: 5px;
+    margin: 3px;
+    &:hover {
+        background-color: antiquewhite;
+        cursor: pointer;
+    }
+`
